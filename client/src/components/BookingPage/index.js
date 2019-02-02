8a24734266b79d5267d4c0fe6335b6d6ca82ba06
ugogo <hello@onali-ugo.fr>
2019-02-02 18:16:58 +0100

    Add a banner saying the trip is not reserved yet

--- client/src/components/BookingPage/index.js
+++ client/src/components/BookingPage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Alert, majorScale } from "evergreen-ui";
 
 import Container from "../Container";
 import Header from "../Header";
@@ -10,6 +11,12 @@ const BookingPage = ({ location: { state } }) => (
     <Header />
 
     <Container paddingY="2rem" backgroundColor="#fafafa">
+      <Alert
+        intent="warning"
+        title="Your trip is not reserved yet!"
+        marginBottom={majorScale(2)}
+      />
+
       <TripItem {...state.ticket} canSelectTicket={false} />
     </Container>
   </div>
