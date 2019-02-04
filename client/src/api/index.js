30a91edacc7560e0a450927a0aa71854f7b17f41
ugogo <hello@onali-ugo.fr>
2019-02-04 18:59:17 +0100

    Update dev api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,19 @@
+import { formatDataForApi, sleep } from "../helpers";
 import { LOCATIONS, TRIPS } from "../constants";
 
-export const fetchLocations = () =>
-  new Promise(resolve => setTimeout(() => resolve(LOCATIONS), 700));
+const getLocationById = id => LOCATIONS.find(({ id: _id }) => _id === id);
 
-export const fetchTrips = () =>
-  new Promise(resolve => setTimeout(() => resolve(TRIPS), 1200));
+export const fetchTrips = async params =>
+  new Promise(async resolve => {
+    const { from, to } = formatDataForApi(params);
+
+    const trips = TRIPS.map(trip => ({
+      ...trip,
+      from: getLocationById(from),
+      to: getLocationById(to),
+    }));
+
+    await sleep(1500);
+
+    resolve(trips);
+  });
