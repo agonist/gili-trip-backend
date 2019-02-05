a4ab3cda418a826f90f09f8ef21e9c1f6ef8200b
billey_b <bastien.billey@gmail.com>
2019-02-05 13:32:09 +0100

    seeds update DAMNNNN

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,21 +26,21 @@ if Rails.env.development?
 
   DateRange.create(from: '2019-02-02', to: '2019-02-06', operator_id: 2)
 
-  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "10:30", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "15:30", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: '5')
+  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "10:30", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "15:30", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: 1)
 
-  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "10:30", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:45", arrival_time:  "16:00", duration: '90', vehicle_id: '5', high_season: true)
+  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "10:30", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:45", arrival_time:  "16:00", duration: '90', vehicle_id: 1, high_season: true)
 
-  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:00", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:15", arrival_time:  "18:00", duration: '90', vehicle_id: '5', high_season: true)
+  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:00", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:15", arrival_time:  "18:00", duration: '90', vehicle_id: 1, high_season: true)
 
-  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:30", arrival_time:  "13:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:30", arrival_time:  "18:00", duration: '90', vehicle_id: '5', high_season: true)
+  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:30", arrival_time:  "13:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:30", arrival_time:  "18:00", duration: '90', vehicle_id: 1, high_season: true)
 
 end
 
@@ -64,21 +64,21 @@ if Rails.env.staging? || Rails.env.production?
 
   DateRange.create(from: '2019-02-02', to: '2019-02-06', operator_id: 1)
 
-  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "10:30", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "15:30", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: '5')
+  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "10:30", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Gili T', from_id: 1, to_id: 3, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "15:30", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Gili A', from_id: 1, to_id: 4, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "09:00", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Bali to Lombok', from_id: 1, to_id: 2, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:00", arrival_time:  "16:00", duration: '90', vehicle_id: 1)
 
-  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "10:30", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:45", arrival_time:  "16:00", duration: '90', vehicle_id: '5', high_season: true)
+  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "10:30", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Gili T to Bali', from_id: 3, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "14:45", arrival_time:  "16:00", duration: '90', vehicle_id: 1, high_season: true)
 
-  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:00", arrival_time:  "11:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:15", arrival_time:  "18:00", duration: '90', vehicle_id: '5', high_season: true)
+  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:00", arrival_time:  "11:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Gili A to Bali', from_id: 4, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:15", arrival_time:  "18:00", duration: '90', vehicle_id: 1, high_season: true)
 
-  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:30", arrival_time:  "13:00", duration: '90', vehicle_id: '5')
-  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:30", arrival_time:  "18:00", duration: '90', vehicle_id: '5', high_season: true)
+  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "11:30", arrival_time:  "13:00", duration: '90', vehicle_id: 1)
+  Trip.create(name: 'Lombok to Bali', from_id: 2, to_id: 1, operator_id: 1, status: 'available', price: '35', currency: "$", departure_time:  "16:30", arrival_time:  "18:00", duration: '90', vehicle_id: 1, high_season: true)
 
 
 end
