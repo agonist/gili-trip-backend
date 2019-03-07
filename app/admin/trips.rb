325feab1ce2dbe0ec4cf36c581594598634ef5ba
billey_b <bastien.billey@gmail.com>
2019-03-07 12:08:53 +0100

    display in admin high season price trip

--- app/admin/trips.rb
+++ app/admin/trips.rb
@@ -1,15 +1,18 @@
 ActiveAdmin.register Trip do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
+
  permit_params :name, :from_id, :to_id, :operator_id, :status, :price, :currency, :departure_time, :arrival_time, :duration, :vehicle_id, :high_season_percentage_multiplier
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+index do
+    (Trip.column_names).each do |c|
+      column c.to_sym
+    end
+    column :high_season_price do |trip|
+      increase = trip.price * (trip.high_season_percentage_multiplier.to_f/100.0)
+      price= trip.price + increase
+      raw price
+    end
+   end
+
+
 
 end
