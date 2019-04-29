88754a311d67933c6780ae4161bdba84e27ff704
billey_b <bastien.billey@gmail.com>
2019-04-29 11:52:14 +0545

    admin stuff

--- app/admin/trips.rb
+++ app/admin/trips.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Trip do
 
- permit_params :name, :from_id, :to_id, :operator_id, :price, :currency, :departure_time, :arrival_time, :duration, :vehicle_id, :high_season_percentage_multiplier
+ permit_params :name, :from_id, :to_id, :operator_id, :price, :currency, :departure_time, :arrival_time, :duration, :vehicle_id, :high_season_percentage_multiplier, :has_pickup, :has_dropoff
 
 
 end
