7114c3a3c3d64f443a169266467640abc089a123
ugogo <hello@onali-ugo.fr>
2019-05-05 09:25:18 +0200

    wrap TicketsTable into an Item

--- client/src/components/TicketsTable.js
+++ client/src/components/TicketsTable.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import dateFns from "date-fns";
 import { Badge, Pane, Table, Text } from "evergreen-ui";
 
+import Item from "./Item";
 import { CURRENCY_SYMBOL, ITEM_SPACE } from "../constants";
 
 const dateFormat = "dddd DD MMMM YYYY [at] hh[:]mma";
@@ -33,74 +34,71 @@ const TicketsTable = ({
   coupon,
   ...props
 }) => (
-  <Table
-    width="100%"
-    backgroundColor="#fff"
-    borderTop="1px solid #E4E7EB"
-    borderLeft="1px solid #E4E7EB"
-    borderRight="1px solid #E4E7EB"
-    {...props}
-  >
-    <Table.Head paddingX={rowProps.padding} paddingY={rowProps.padding * 1.5}>
-      <Table.TextHeaderCell flexGrow={1}>Your tickets</Table.TextHeaderCell>
-      <Table.TextHeaderCell {...mediumColProps}>Operator</Table.TextHeaderCell>
-      <Table.TextHeaderCell {...smallColProps}>Quantity</Table.TextHeaderCell>
-      <Table.TextHeaderCell {...smallColProps}>Price</Table.TextHeaderCell>
-      <Table.TextHeaderCell {...smallColProps}>Subtotal</Table.TextHeaderCell>
-    </Table.Head>
+  <Item padding={0} overflow="hidden" {...props}>
+    <Table width="100%" backgroundColor="#fff">
+      <Table.Head paddingX={rowProps.padding} paddingY={rowProps.padding * 1.5}>
+        <Table.TextHeaderCell flexGrow={1}>Your tickets</Table.TextHeaderCell>
+        <Table.TextHeaderCell {...mediumColProps}>
+          Operator
+        </Table.TextHeaderCell>
+        <Table.TextHeaderCell {...smallColProps}>Quantity</Table.TextHeaderCell>
+        <Table.TextHeaderCell {...smallColProps}>Price</Table.TextHeaderCell>
+        <Table.TextHeaderCell {...smallColProps}>Subtotal</Table.TextHeaderCell>
+      </Table.Head>
 
-    <Table.Body>
-      {tickets.map(({ id, trip_id, from, to, date, price, operator }) => (
-        <Table.Row key={id || trip_id} {...rowProps}>
-          <Table.TextCell flexGrow={1}>
-            {`${from.name} -> ${to.name}`}
-            <br />
-            <strong>{dateFns.format(date, dateFormat)}</strong>
-          </Table.TextCell>
-          <Table.TextCell {...mediumColProps}>{operator.name}</Table.TextCell>
-          <Table.TextCell {...smallColProps}>x{quantity}</Table.TextCell>
-          <Table.TextCell {...smallColProps}>
-            {`${price}${CURRENCY_SYMBOL}`}
-          </Table.TextCell>
-          <Table.TextCell {...smallColProps}>
-            {`${price * quantity}${CURRENCY_SYMBOL}`}
+      <Table.Body>
+        {tickets.map(({ id, trip_id, from, to, date, price, operator }) => (
+          <Table.Row key={id || trip_id} {...rowProps}>
+            <Table.TextCell flexGrow={1}>
+              {`${from.name} -> ${to.name}`}
+              <br />
+              <strong>{dateFns.format(date, dateFormat)}</strong>
+            </Table.TextCell>
+            <Table.TextCell {...mediumColProps}>{operator.name}</Table.TextCell>
+            <Table.TextCell {...smallColProps}>x{quantity}</Table.TextCell>
+            <Table.TextCell {...smallColProps}>
+              {`${price}${CURRENCY_SYMBOL}`}
+            </Table.TextCell>
+            <Table.TextCell {...smallColProps}>
+              {`${price * quantity}${CURRENCY_SYMBOL}`}
+            </Table.TextCell>
+          </Table.Row>
+        ))}
+
+        <Table.Row {...rowProps}>
+          <Table.TextCell textAlign="right">
+            <Pane>
+              Total:{" "}
+              {final_price !== full_price && (
+                <span
+                  style={{
+                    display: "inline-block",
+                    textDecoration: "line-through",
+                    margin: "0 4px",
+                  }}
+                >
+                  {full_price}
+                  {CURRENCY_SYMBOL}
+                </span>
+              )}
+              <Text>
+                <strong>
+                  {final_price}
+                  {CURRENCY_SYMBOL}
+                </strong>
+              </Text>
+            </Pane>
+
+            {coupon && (
+              <Badge color="green" isSolid marginTop={4}>
+                {coupon.code}
+              </Badge>
+            )}
           </Table.TextCell>
         </Table.Row>
-      ))}
-
-      <Table.Row {...rowProps}>
-        <Table.TextCell textAlign="right">
-          <Pane>
-            Total:{" "}
-            {final_price !== full_price && (
-              <span
-                style={{
-                  display: "inline-block",
-                  textDecoration: "line-through",
-                  margin: "0 4px",
-                }}
-              >
-                {full_price}
-                {CURRENCY_SYMBOL}
-              </span>
-            )}
-            <Text>
-              <strong>
-                {final_price}
-                {CURRENCY_SYMBOL}
-              </strong>
-            </Text>
-          </Pane>
-
-          {coupon && (
-            <Badge color="green" isSolid marginTop={4}>
-              {coupon.code}
-            </Badge>
-          )}
-        </Table.TextCell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
+      </Table.Body>
+    </Table>
+  </Item>
 );
 
 TicketsTable.propTypes = {
