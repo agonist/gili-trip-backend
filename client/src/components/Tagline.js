de5b5283fa6fd8c892a112bf7aeff521c1a0ff52
ugogo <hello@onali-ugo.fr>
2019-05-12 18:53:42 +0200

    fix icons alignment

--- client/src/components/Tagline.js
+++ client/src/components/Tagline.js
@@ -10,7 +10,7 @@ import { ITEM_SPACE } from "../constants";
 const items = [
   {
     icon: "tag",
-    title: "Find the best price for your journey",
+    title: "Find the best price",
     text:
       "Compare prices of the different boat trip instantly, no need to bargain!",
   },
@@ -40,24 +40,24 @@ const Tagline = () => {
         {items.map(({ icon, title, text }) => (
           <Pane
             key={title}
-            display="flex"
-            flexDirection="row"
             flex={1}
             padding={ITEM_SPACE}
             paddingLeft={ITEM_SPACE / 2}
           >
-            <Icon
-              icon={icon}
-              size={ITEM_SPACE}
-              marginTop={ITEM_SPACE / 2}
-              color="#34d1b6"
-              flexShrink={0}
-            />
-
-            <Pane paddingLeft={ITEM_SPACE}>
-              <Heading>{title}</Heading>
-              <P marginTop={isMobile ? 6 : ITEM_SPACE / 2}>{text}</P>
+            <Pane display="flex" alignItems="center" justifyContent="center">
+              <Icon
+                icon={icon}
+                size={ITEM_SPACE}
+                color="#34d1b6"
+                flexShrink={0}
+                marginRight={ITEM_SPACE}
+              />
+              <Heading flexGrow={1}>{title}</Heading>
             </Pane>
+
+            <P paddingLeft={ITEM_SPACE * 2} marginTop={ITEM_SPACE / 2}>
+              {text}
+            </P>
           </Pane>
         ))}
       </Pane>
