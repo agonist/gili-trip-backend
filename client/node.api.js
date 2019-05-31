86e375ae6d2fd22c3e06a3805ffadf3df9401972
ugogo <hello@onali-ugo.fr>
2019-05-31 20:01:04 +0200

    test

--- client/node.api.js
+++ client/node.api.js
@@ -5,13 +5,15 @@ export default () => ({
   headElements: elements => {
     const { css, hydrationScript } = extractStyles();
     const formattedCss = `@charset "utf-8";${css}`;
+
     return [
+      <meta charSet="UTF-8" />,
       ...elements,
+      hydrationScript,
       <style
         id="evergreen-css"
         dangerouslySetInnerHTML={{ __html: formattedCss }}
       />,
-      hydrationScript,
     ];
   },
 });
