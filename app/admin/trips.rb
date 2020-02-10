8967eb614be728f3b7663fd32ed9331b95e25583
billey_b <bastien.billey@gmail.com>
2020-02-10 14:39:45 +0800

    add high season

--- app/admin/trips.rb
+++ app/admin/trips.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Trip do
 
- permit_params :name, :from_id, :to_id, :operator_id, :price, :currency, :departure_time, :arrival_time, :duration, :trips_group_id, :vehicle_id, :high_season_percentage_multiplier, :has_pickup, :has_dropoff, :return_price, :idr_price, :idr_return_price
+ permit_params :name, :from_id, :to_id, :operator_id, :price, :currency, :departure_time, :arrival_time, :duration, :trips_group_id, :vehicle_id, :high_season_percentage_multiplier, :high_season ,:has_pickup, :has_dropoff, :return_price, :idr_price, :idr_return_price
 
 
 end
